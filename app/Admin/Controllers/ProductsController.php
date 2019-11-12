056563bf9fae0608be4671819f81c07a297e2119
杨万里 <1091231787@qq.com>
2019-11-12 17:32:04 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -91,7 +91,8 @@ class ProductsController extends AdminController
         $form->image('image', '封面图片')->rules('required|image');
 
         // 创建一个富文本编辑器
-        $form->editor('description', '商品描述')->rules('required');
+        //$form->editor('description', '商品描述')->rules('required');
+        $form->quill('description', '商品描述')->rules('required');
 
         // 创建一组单选框
         $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
