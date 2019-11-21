f2a9ad1afc49a3bd73cfec547cffa308a5be1ee4
杨万里 <1091231787@qq.com>
2019-11-21 11:55:41 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 //Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
