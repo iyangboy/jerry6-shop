ff7d7cc35d793d8ac7e6783df3c11786f75f8626
杨万里 <1091231787@qq.com>
2019-12-12 17:23:45 +0800

    异常处理

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,11 +9,11 @@ class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
+     * 屏蔽指定异常写日志
      * @var array
      */
     protected $dontReport = [
-        //
+        InvalidRequestException::class,
     ];
 
     /**
