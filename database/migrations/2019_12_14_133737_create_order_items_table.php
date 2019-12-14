cd84c5b40308eb917b78f9c822b159ec856e0a05
杨万里 <1091231787@qq.com>
2019-12-14 13:44:16 +0800

    订单模型

---
+++ database/migrations/2019_12_14_133737_create_order_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id')->comment('所属订单 ID');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id')->comment('对应商品 ID');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->unsignedBigInteger('product_sku_id')->comment('对应商品 SKU ID');
+            $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
+            $table->unsignedInteger('amount')->comment('数量');
+            $table->decimal('price', 10, 2)->comment('单价');
+            $table->unsignedInteger('rating')->nullable()->comment('用户打分');
+            $table->text('review')->nullable()->comment('用户评价');
+            $table->timestamp('reviewed_at')->nullable()->comment('评价时间');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
