1a7e1bbbf33969a330bc6158215dd36a1e3e98e3
杨万里 <1091231787@qq.com>
2019-12-16 11:03:50 +0800

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -66,6 +67,9 @@ class OrdersController extends Controller
 
             return $order;
         });
+
+        // 关闭未支付订单，延迟判断是否已经支付
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
     }
