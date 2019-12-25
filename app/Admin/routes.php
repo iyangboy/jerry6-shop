5f9771a567f68616d4b698cacd9b49a93634b931
杨万里 <1091231787@qq.com>
2019-12-25 17:18:26 +0800

    备份管理后台数据

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -41,4 +41,13 @@ Route::group([
     $router->put('coupon_codes/{id}', 'CouponCodesController@update');
     // 优惠券-删除
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
+
+    // 商品分类
+    $router->get('categories', 'CategoriesController@index');
+    $router->get('categories/create', 'CategoriesController@create');
+    $router->get('categories/{id}/edit', 'CategoriesController@edit');
+    $router->post('categories', 'CategoriesController@store');
+    $router->put('categories/{id}', 'CategoriesController@update');
+    $router->delete('categories/{id}', 'CategoriesController@destroy');
+    $router->get('api/categories', 'CategoriesController@apiIndex');
 });
--- database/admin.sql
+++ database/admin.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for osx10.15 (x86_64)
 --
--- Host: localhost    Database: jerry-shop
+-- Host: 127.0.0.1    Database: jerry6-shop
 -- ------------------------------------------------------
 -- Server version	10.4.6-MariaDB
 
@@ -20,7 +20,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-11-12 04:18:53'),(2,0,8,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-12-24 10:20:58'),(3,2,9,'管理员','fa-users','auth/users',NULL,NULL,'2019-12-24 10:20:58'),(4,2,10,'角色','fa-user','auth/roles',NULL,NULL,'2019-12-24 10:20:58'),(5,2,11,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-12-24 10:20:58'),(6,2,12,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-12-24 10:20:58'),(7,2,13,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-12-24 10:20:58'),(8,0,2,'用户管理','fa-users','/',NULL,'2019-11-12 04:42:28','2019-11-12 04:43:23'),(9,8,3,'用户列表','fa-users','/users',NULL,'2019-11-12 04:43:14','2019-11-12 04:43:23'),(10,0,4,'商品管理','fa-cubes','/',NULL,'2019-11-12 09:05:20','2019-11-12 09:05:59'),(11,10,5,'商品列表','fa-cubes','/products',NULL,'2019-11-12 09:05:49','2019-11-12 09:05:59'),(12,0,6,'订单管理','fa-rmb','/orders',NULL,'2019-12-24 06:58:56','2019-12-24 06:59:19'),(13,0,7,'优惠券管理','fa-tags','/coupon_codes',NULL,'2019-12-24 10:20:49','2019-12-24 10:20:58');
+INSERT INTO `admin_menu` VALUES (1,0,1,'控制台','fa-bar-chart','/',NULL,NULL,'2019-11-12 04:34:26'),(2,0,9,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-12-25 09:13:56'),(3,2,10,'管理员','fa-users','auth/users',NULL,NULL,'2019-12-25 09:13:56'),(4,2,11,'角色','fa-user','auth/roles',NULL,NULL,'2019-12-25 09:13:56'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-12-25 09:13:56'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-12-25 09:13:56'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-12-25 09:13:56'),(8,0,2,'用户管理','fa-user','/',NULL,'2019-11-12 04:48:27','2019-11-12 04:49:37'),(9,8,3,'用户列表','fa-users','/users',NULL,'2019-11-12 04:48:47','2019-11-12 04:49:37'),(10,0,5,'商品管理','fa-cubes','/',NULL,'2019-11-12 09:08:27','2019-12-25 09:13:56'),(11,10,6,'商品列表','fa-cubes','/products',NULL,'2019-11-12 09:08:52','2019-12-25 09:13:56'),(12,0,7,'订单管理','fa-rmb','/orders',NULL,'2019-12-24 06:57:34','2019-12-25 09:13:56'),(13,0,8,'优惠券管理','fa-tags','/coupon_codes',NULL,'2019-12-25 01:36:02','2019-12-25 09:13:56'),(14,0,4,'商品分类管理','fa-bars','/categories',NULL,'2019-12-25 09:13:40','2019-12-25 09:13:56');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -30,7 +30,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_permissions` WRITE;
 /*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
-INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'用户管理','users','','/users*','2019-11-12 08:48:14','2019-11-12 08:48:14'),(7,'商品管理','products','','/products*','2019-12-25 03:52:48','2019-12-25 03:52:48'),(8,'优惠券管理','coupon_codes','','/coupon_codes*','2019-12-25 03:53:47','2019-12-25 03:53:47'),(9,'订单管理','orders','','/orders*','2019-12-25 03:54:13','2019-12-25 03:54:20');
+INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'用户管理','users','','/users*','2019-11-12 08:52:06','2019-11-12 08:52:06'),(7,'商品管理','products','','/products*','2019-12-25 06:26:26','2019-12-25 06:26:26'),(8,'优惠券管理','coupon_codes','','/coupon_codes*','2019-12-25 06:27:10','2019-12-25 06:27:10'),(9,'订单管理','orders','','/orders*','2019-12-25 06:27:44','2019-12-25 06:27:44');
 /*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +70,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_roles` WRITE;
 /*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
-INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2019-11-12 03:40:26','2019-11-12 03:40:26'),(2,'测试','test','2019-11-12 08:49:40','2019-11-12 08:49:40'),(3,'运营','operator','2019-12-25 03:57:33','2019-12-25 03:57:33');
+INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2019-11-12 04:22:19','2019-11-12 04:22:19'),(2,'测试','test','2019-11-12 08:52:43','2019-11-12 08:52:43'),(3,'运营','operator','2019-12-25 06:28:46','2019-12-25 06:28:46');
 /*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +89,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_users` WRITE;
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
-INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$rDmwmBxT/CO6bTAt/amfNuQwpOs.Yk7hiCvyQzJ19Y9izDzoSYmUK','Administrator',NULL,'fvT7Vpv6ji32lR4gyQZE72DNaBjcQZ2daRZnzGnLDdLn3vQPtCGhtTOrGPNu','2019-11-12 03:40:26','2019-11-12 03:40:26'),(2,'test','$2y$10$TLl4UfaW7l4hlBC.9TBrJ.CpvFmxRdl7xlEslXuoY.MdS6F4b//WW','测试',NULL,'F1p9JXRpWkXhpD2CN1LjLwTMZy5B4tHbno5tIGBNZlqHcL3J6S3bRN9maVRe','2019-11-12 08:50:47','2019-11-12 08:50:47'),(3,'operator','$2y$10$kLG4bxgmN2liyJi1xm7ghuo5JbH59IoQDz/g56qIZGf1cTgoGRPJG','运营',NULL,'gUOPGidDZlg0gEHZgjNFpqa1fKY06utVj68vGrxAenprRdva6mf1vrOi0FBy','2019-12-25 03:55:39','2019-12-25 03:55:39');
+INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$z05bekBGwLjL7i0gXh68U.Q.4VqdKVP/3Gk2dtvxe4Yh8PylINJ9a','Administrator',NULL,'ZNDfWQfJirsHboYIg8hkjgYR8hTR5dwSNEnSAYmhc6pvmwOIVnIIv1v5qCBY','2019-11-12 04:22:19','2019-11-12 04:22:19'),(2,'test','$2y$10$T66es1uxdwvBkszIeSKxOezA3BB563U9mhldTsMKcF0bWQvf2KECK','测试',NULL,'UmKmmv7bzC8LNuVVJalbux5N6crUDsNyQoGJ92LICSmPCyqjHmLshSXG0ZX4','2019-11-12 08:53:08','2019-11-12 08:53:08'),(3,'operator','$2y$10$KDd697s4U/LXpMlfoS5f4u7q2WIfS67ecdTrX9N1O95Cqad/OfGIK','运营','images/92a4fe51dba5f951dacc16cb8e9883c8.png','0bZGp2WrnXmV3xfgxTEPFGxMpDdJdcLUUT2Y4ZXH7bcMeLIfxnBMVZDgjXG1','2019-12-25 06:29:24','2019-12-25 06:29:24');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -101,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-25 14:38:52
+-- Dump completed on 2019-12-25 17:17:35
