357bc82915a419afad70d3c3d93656f75cbcb8e2
杨万里 <1091231787@qq.com>
2019-12-30 17:02:35 +0800

    使用 JWT

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
@@ -10,6 +11,30 @@ use Illuminate\Support\Arr;
 
 class AuthorizationsController extends Controller
 {
+    // 登录
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        // return response()->json([
+        //     'access_token' => $token,
+        //     'token_type' => 'Bearer',
+        //     'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        // ])->setStatusCode(201);
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
     // 微信登录
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
@@ -55,6 +80,33 @@ class AuthorizationsController extends Controller
             break;
         }
 
-        return response()->json(['token' => $user->id]);
+        $token= auth('api')->login($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+
+        // return response()->json(['token' => $user->id]);
+    }
+
+    // 返回登录认证信息
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    // 刷新token
+    public function update()
+    {
+        $token = auth('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    // 删除token
+    public function destroy()
+    {
+        auth('api')->logout();
+        return response(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -26,25 +26,28 @@ Route::prefix('v1')
         return 'this is version v1';
     })->name('version');
 
-    Route::middleware('throttle:' . config('api.rate_limits.sign'))
-    ->group(function () {
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function () {
         // 图片验证码
         Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
         // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
         // 用户注册
         Route::post('users', 'UsersController@store')->name('users.store');
-        // 第三方登录
+        // 第三方登录-微信
         Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
             ->where('social_type', 'weixin')
             ->name('socials.authorizations.store');
+        // 登录
+        Route::post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
+
+        // 刷新token
+        Route::put('authorizations/current', 'AuthorizationsController@update')->name('authorizations.update');
+        // 删除token
+        Route::delete('authorizations/current', 'AuthorizationsController@destroy')->name('authorizations.destroy');
     });
-    Route::middleware('throttle:' . config('api.rate_limits.access'))
-    ->group(function () {
-        // 第三方登录
-        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-            ->where('social_type', 'weixin')
-            ->name('api.socials.authorizations.store');
+
+    //
+    Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
     });
 
 });
