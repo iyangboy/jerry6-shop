6f962936daa87cf2e324dc516dd7b89ab3a5e7b6
杨万里 <1091231787@qq.com>
2019-12-30 11:00:37 +0800

    API 基础配置

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->name('api.v1.')->group(function() {
+    Route::get('version', function() {
+        abort(403, 'test');
+        return 'this is version v1';
+    })->name('version');
+});
+
+Route::prefix('v2')->name('api.v2.')->group(function() {
+    Route::get('version', function() {
+        return 'this is version v2';
+    })->name('version');
+});
