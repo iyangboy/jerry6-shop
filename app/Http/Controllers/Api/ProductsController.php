f11b1d910f50bdffad1e0ddd6d513c695a9deaf2
杨万里 <1091231787@qq.com>
2019-12-31 13:53:33 +0800

    商品列表

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProductsController extends Controller
 {
@@ -12,14 +14,24 @@ class ProductsController extends Controller
     // 产品列表
     public function index(Request $request, Product $product)
     {
-        $query = $product->query();
+        // $query = $product->query();
 
-        if ($categoryId = $request->category_id) {
-            $query->where('category_id', $categoryId);
-        }
+        // if ($categoryId = $request->category_id) {
+        //     $query->where('category_id', $categoryId);
+        // }
 
-        $products = $query->with('category')->paginate();
+        // $products = $query->with('category')->paginate();
 
+        // return ProductResource::collection($products);
+
+        $products = QueryBuilder::for(Product::class)
+            ->allowedIncludes('category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+            ])
+            ->paginate();
         return ProductResource::collection($products);
+
     }
 }
